d78f2d926d99307f905dbc7994f946adc1841dce
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-05-22 11:53:11 -0400

    Foundation in Activity 3

--- 03-CSS-Frameworks/Unsolved/src/App.js
+++ 03-CSS-Frameworks/Unsolved/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button, TopBar, TopBarTitle } from "react-foundation";
 
 function App() {
-	return (
-		<></>
-	);
+  return (
+    <>
+      <TopBar>
+        <TopBarTitle>Welcome to My Site</TopBarTitle>
+      </TopBar>
+      Hello world.
+      <Button>This is my button</Button>
+    </>
+  );
 }
 
 export default App;
